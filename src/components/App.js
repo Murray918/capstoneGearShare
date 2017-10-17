3c9fd3abe06c8f088bb7fd17ccc380c6b659d0b8
Andrew Murray <darbyguitars@gmail.com>
2017-10-17 09:30:26 -0500

    got the basic scafold set up

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>give me a little drink from your lovin cup</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
