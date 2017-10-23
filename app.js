0b01a0538b6aafdddce0067513ad434aed697c0f
Andrew Murray <darbyguitars@gmail.com>
2017-10-23 17:13:46 -0500

    adding some functionality the app

--- app.js
+++ app.js
@@ -1,84 +1,118 @@
-const models = require('./models'),
-      Sequelize = require('sequelize'),
-      express = require('express'),
-      bodyParser = require('body-parser');
+const models = require("./models"),
+  Sequelize = require("sequelize"),
+  express = require("express"),
+  bodyParser = require("body-parser");
 let app = express();
 
 //boiler plate body parser can talk to the html and get it recieve it in app.js
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-console.log('party party')
+console.log("party party");
 
 //this creates the table for all the products
-function createProduct(){
+function createProduct() {
   const product = models.Product.build({
-    make:'Sure',
-    modelNum:'SM58',
-    info:'this is a micrphpne for live sound'
+    make: "Sure",
+    modelNum: "SM7B",
+    info: "this is a micrphpne spoken word"
     // tags: ['mic', 'micrphpne', 'live']
   });
-  product.save().then(function(newProduct){
+  product.save().then(newProduct => {
     console.log(newProduct.dataValues);
-  })
-  }
+  });
+}
+// createProduct();
 
-
-  // createProduct();
-
-  function listProducts() {
-
-  models.Product.findAll().then(function (products) {
-    products.forEach(function(product){
+function listProducts() {
+  models.Product.findAll().then(function(products) {
+    products.forEach(function(product) {
       console.log(product.dataValues);
-    })
-  })
-  }
-
-  listProducts();
-
-  function createUser(){
-    const user = models.User.build({
-      name:'Bailey Bryant',
-      userName :'bkittyface',
-      bio : 'I like cats you fool'
-      // tags: ['mic', 'micrphpne', 'live']
     });
+  });
+}
 
-  user.save().then(function(newUser){
+listProducts();
+
+function deleteProduct () {
+  models.Product.findById(3)
+    .then( (product) => {
+      console.log(product);
+      product.destroy()
+    })
+}
+
+deleteProduct();
+
+function createUser() {
+  const user = models.User.build({
+    name: "Sarah Shuey",
+    userName: "fluffyPuppy",
+    bio: "I like dogs you fool"
+    // tags: ['mic', 'micrphpne', 'live']
+  });
+
+  user.save().then(function(newUser) {
     console.log(newUser.dataValues);
-  })
-  }
+  });
+}
 
 // createUser();
 
 function listUsers() {
-
-models.User.findAll().then(function (users) {
-  users.forEach(function(user){
-    console.log(user.dataValues);
-  })
-})
+  models.User.findAll().then(function(users) {
+    users.forEach(function(user) {
+      console.log(user.dataValues);
+    });
+  });
 }
 
 listUsers();
 
-app.listen(3000, function() {
-  console.log('successfully started Express Application');
-})
+function updateUser() {
+  models.User
+    .update(
+      {
+        name: "Bailey Marie"
+      },
+      {
+        where: {
+          id: 1
+        }
+      }
+    )
+    .then(result => {
+      models.User.findById(1).then(user => {
+        // console.log(user);
+      });
+    });
+}
 
-process.on('SIGINT', function() {
+updateUser();
+
+function deleteUser (){
+  models.User.findById(3)
+    .then( (user) => {
+      console.log(user);
+      user.destroy()
+    })
+}
+
+// deleteUser();
+
+app.listen(8080, () => {
+  console.log("successfully started Express Application");
+});
+
+process.on("SIGINT", () => {
   console.log("\nshutting down");
-  const index = require('./models/index')
-  index.sequelize.close()
+  const index = require("./models/index");
+  index.sequelize.close();
 
   // give it a second
-  setTimeout(function() {
-    console.log('process exit');
+  setTimeout(() => {
+    console.log("process exit");
     process.exit(0);
-  }, 1000)
+  }, 1000);
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+// import
 
-class App extends Component {
+
+function logItTest (){
+  console.log('bang');
+}
+
+
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>give me a little drink from your lovin cup</h1>
+        <h1>Give me a little drink from your lovin cup</h1>
+        <button onClick ={logItTest}>TEST CREATE</button>
       </div>
     );
   }
 }
-
-export default App;
